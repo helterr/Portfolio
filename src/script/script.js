a6384422d7edd86c651fd9de572fceab46ea60ca
Helter Rondon <139646776+helterr@users.noreply.github.com>
2023-07-29 12:43:32 -0400

    add active function on navbar links

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,18 @@
-navlink = document.querySelector("")
+let navlinks = document.querySelectorAll('.navbar a');
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('.navbar a[href*=' + id + ']').classList.add('active');
+        }
+    });
+};
